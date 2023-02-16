bd5e4a776743d9098d34c80a341c712da2511f1b
kobadai0206 <kobadai0206@gmail.com>
2023-02-16 22:47:18 +0900

    first commit

---
+++ resources/views/components/layout.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+  <link rel="stylesheet" href="{{ mix('/css/app.css') }}">
+  <script src="{{ mix('/js/app.js') }}"></script>
+  <title>{{ $title ?? 'つぶやきアプリ' }}</title>
+  @stack('css')
+</head>
+<body class="bg-gray-50">
+  {{ $slot }}
+</body>
+</html>
---
+++ webpack.mix.js
@@ -0,0 +1,10 @@
+// webpack.mix.js
+
+let mix = require('laravel-mix');
+
+mix.js('resources/js/app.js', 'public/js').postCss('resources/css/app.css', 'public/css', [
+    require('tailwindcss'),
+  ]);
+  if (mix.inProduction()) {
+    mix.version();
+  }
